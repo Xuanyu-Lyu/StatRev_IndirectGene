c59508b40078f69320f62770a0c0b8e0bfa90a61
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2025-09-17 17:17:07 -0600

    Update simulation conditions and SLURM array size for accuracy in run_simulations.py and submit_simulations_hpc.sh

--- Scripts/01-Simulation/submit_simulations_hpc.sh
+++ Scripts/01-Simulation/submit_simulations_hpc.sh
@@ -12,8 +12,8 @@
 #SBATCH --error=slurm_logs/sim_run_%A_%a.err
 
 # --- Define the total number of tasks for the array ---
-# CORRECTED: (5 conditions * 1000 reps) / 4 reps_per_task = 1250 tasks
-#SBATCH --array=1-1250%25
+# CORRECTED: (4 conditions * 1000 reps) / 4 reps_per_task = 1000 tasks
+#SBATCH --array=1-1000%25
 
 # --- Your Job's Commands ---
 
